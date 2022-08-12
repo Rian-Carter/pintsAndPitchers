b526de7e699799bd64e054a3d9f95af2574631e5
Rian Carter <R.Carter86@hotmail.com>
2022-08-12 12:54:58 -0700

    update proptypes to take in id and key and remove index as a parameter

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -6,20 +6,23 @@ function KegList(props){
   return (
     <React.Fragment>
       <hr />
-      {props.kegList.map((keg, index) =>
+      {props.kegList.map((keg) =>
         <Keg
+          whenKegClick = { props.onKegSelection }
           name={keg.name}
           origin={keg.origin}
           price={keg.price}
           abv={keg.abv}
-          key={index}/>
+          id={keg.id}
+          key={keg.id}/>
       )}
     </React.Fragment>
   );
 }
 
 KegList.propTypes = {
-  kegList: PropTypes.array
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
 };
 
 export default KegList;
