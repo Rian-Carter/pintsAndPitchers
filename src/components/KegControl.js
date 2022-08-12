2dcc2918f966557f7d8f931c9010a23959ca8384
Rian Carter <R.Carter86@hotmail.com>
2022-08-12 12:43:45 -0700

    add handlechangingselectedkeg function

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -8,7 +8,8 @@ class KegControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      mainKegList: []
+      mainKegList: [],
+      selectedKeg: null
     };
   }
 
@@ -22,6 +23,11 @@ class KegControl extends React.Component {
     const newMainKegList = this.state.mainKegList.concat(newKeg);
     this.setState({mainKegList: newMainKegList,
                   formVisibleOnPage: false });
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.mainKegList.filter(keg => keg.id === id)[0];
+    this.setState({selectedKeg: selectedKeg});
   }
 
   render(){
