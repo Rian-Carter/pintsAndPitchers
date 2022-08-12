321612a304913b03fa49b5dfc36ac988699630b0
Rian Carter <R.Carter86@hotmail.com>
2022-08-12 13:02:29 -0700

    add proptypes and actual details of kegs

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function KegDetail(props){
+  const { keg } = props;
+
   return (
     <React.Fragment>
       <h1>Keg Detail</h1>
+      <h3>{keg.origin} - {keg.name} - {keg.price}</h3>
+      <p><em>{keg.abv}</em></p>
       <hr />
     </React.Fragment>
   );
 }
+
+KegDetail.propTypes = {
+  keg: PropTypes.object
+};
 
 export default KegDetail;
