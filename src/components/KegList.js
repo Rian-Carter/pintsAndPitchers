459c5d19e439706685a894d83906cf1bbf3275b2
Rian Carter <R.Carter86@hotmail.com>
2022-08-12 12:20:13 -0700

    import proptypes, update return statement to take array

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,32 +1,12 @@
 import React from 'react';
 import Keg from './Keg';
+import PropTypes from 'prop-types';
 
-const mainKegList = [
-  {
-    name: 'Wanderlust',
-    origin: 'Portland, OR',
-    price: '$6.50',
-    abv: '6.5%'
-  },
-  {
-    name: 'Johnny Utah',
-    origin: 'Bend, OR',
-    price: '$6.50',
-    abv: '5.5%'
-  },
-  {
-    name: 'Belching Beaver',
-    origin: 'Seattle, WA',
-    price: '$6.50',
-    abv: '3.7%'
-  }
-];
-
-function KegList(){
+function KegList(props){
   return (
     <React.Fragment>
       <hr />
-      {mainKegList.map((keg, index) =>
+      {props.kegList.map((keg, index) =>
         <Keg
           name={keg.name}
           origin={keg.origin}
@@ -37,5 +17,9 @@ function KegList(){
     </React.Fragment>
   );
 }
+
+KegList.propTypes = {
+  kegList: PropTypes.array
+};
 
 export default KegList;
