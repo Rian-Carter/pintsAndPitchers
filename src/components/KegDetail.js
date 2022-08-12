735ccec2c455c37ec9a30df8840e97499c22852d
Rian Carter <R.Carter86@hotmail.com>
2022-08-12 13:17:19 -0700

    add destroy functionality

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,20 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function KegDetail(props){
-  const { keg } = props;
+  const { keg, onClickingDelete } = props;
 
   return (
     <React.Fragment>
       <h1>Keg Detail</h1>
       <h3>{keg.origin} - {keg.name} - {keg.price}</h3>
       <p><em>{keg.abv}</em></p>
+      <button onClick={()=> onClickingDelete(ticket.id) }>Delete Keg</button>
       <hr />
     </React.Fragment>
   );
 }
 
 KegDetail.propTypes = {
-  keg: PropTypes.object
+  keg: PropTypes.object,
+  onClickingDelete: PropTypes.func
 };
 
 export default KegDetail;
