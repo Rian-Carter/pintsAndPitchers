2b96039891639c35fd4d03ed73e2b9659552fa4e
Rian Carter <R.Carter86@hotmail.com>
2022-08-12 11:15:44 -0700

    add array of kegs

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 function Keg(props){
   return (
     <React.Fragment>
-      <h3>{props.location} - {props.names}</h3>
-      <p><em>{props.issue}</em></p>
+      <h3>{props.location} - {props.beer}</h3>
+      <p><em>{props.status}</em></p>
       <hr />
     </React.Fragment>
   );
 }
+
+Keg.propTypes = {
+  beer: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired
+};
 
 export default Keg;
--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import Keg from './Keg';
 
+const mainKegList = [
+  {
+    beer: 'Wanderlust',
+    location: 'Portland, OR',
+    status: '50%'
+  },
+  {
+    beer: 'Johnny Utah',
+    location: 'Bend, OR',
+    status: '73%'
+  },
+  {
+    beer: 'Belching Beaver',
+    location: 'Seattle, WA',
+    status: '24%'
+  }
+];
+
 function KegList(){
   return (
-    <Keg 
-      location="Portalnd, OR"
-      names="Wanderlust and Johnny Utah"
-      issue="Kegs are running low"/>
+    <React.Fragment>
+      <Keg 
+        location="Portalnd, OR"
+        beer="Wanderlust"
+        status="Kegs are running low"/>
+      <Keg
+        location="Bend, OR"
+        beer="Johnny Utah"
+        status="Topped Up"/>
+    </React.Fragment>
   );
 }
 
