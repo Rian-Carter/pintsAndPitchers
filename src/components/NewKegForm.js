44fd904e49773ab2db0dad298d79b14e65766e3a
Rian Carter <R.Carter86@hotmail.com>
2022-08-12 12:09:48 -0700

    add form with all inputs and start adding handleNewKegFormSubmission function

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { v4 } from 'uuid';
 
-function NewKegForm(props){
+function NewKegForm(){
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.origin.value);
+    console.log(event.target.price.value);
+    console.log(event.target.abv.value);
+  }
+  
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name' />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Origin' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price' />
+        <input
+          type='text'
+          name='abv'
+          placeholder='%5.5' />
+        <button type='submit'>Add Keg</button>
+      </form>
     </React.Fragment>
   );
 }
