fba5eac425b33e633c45f23c7516f67454a5a786
Rian Carter <R.Carter86@hotmail.com>
2022-08-12 13:19:34 -0700

    refactor return statement to take in reusable form

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { v4 } from 'uuid';
 import PropTypes from 'prop-types';
+import ReusableForm from './ReusableForm';
 
 function NewKegForm(props){
 
@@ -17,25 +18,9 @@ function NewKegForm(props){
 
   return (
     <React.Fragment>
-      <form onSubmit={handleNewKegFormSubmission}>
-        <input
-          type='text'
-          name='name'
-          placeholder='Name' />
-        <input
-          type='text'
-          name='origin'
-          placeholder='Origin' />
-        <input
-          type='text'
-          name='price'
-          placeholder='Price' />
-        <input
-          type='text'
-          name='abv'
-          placeholder='%5.5' />
-        <button type='submit'>Add Keg</button>
-      </form>
+      <ReusableForm
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Add Keg" />
     </React.Fragment>
   );
 }
